612d5e8c7422efdcca920de81d0f0c6da4b56e20
akashavudaiyappan <48153613+akashavudaiyappan@users.noreply.github.com>
2019-03-02 17:50:34 +0530

    Create find sign.c

---
+++ find sign.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+ printf("The Number is positive");
+else 
+ if(a<0)
+  printf("The Number is Negative");
+ else 
+  printf("zero");
+getch();
+
+}
